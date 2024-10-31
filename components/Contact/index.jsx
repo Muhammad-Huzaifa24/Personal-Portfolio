372261d330cf8f848e4257b174f0b3fe7a5e6074
Muhammad <huzaifakhawar100@gmail.com>
2024-10-31 22:20:21 +0500

    CHANGE font size in contact text-xl

--- components/Contact/index.jsx
+++ components/Contact/index.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
 			className="py-8 pb-0 px-2 max-w-6xl m-auto sm:py-20"
 			id="contact"
 		>
-			<h1 className="text-3xl text-teal-500 font-bold sm:text-5xl">
+			<h1 className="text-4xl text-teal-500 font-bold sm:text-5xl">
 				Feel free, <br /> Say hi;
 			</h1>
 			<div className="my-10 grid grid-cols-2 gap-0 lg:grid-cols-5 sm:grid-cols-3 sm:gap-4">
@@ -60,7 +60,7 @@ const Contact = () => {
 				</ul>
 
 				<div className="mt-10 sm:mt-0">
-					<h2 className="text-1xl text-teal-500 font-bold mb-2 sm:text-3xl">
+					<h2 className="text-xl text-teal-500 font-bold mb-2 sm:text-3xl">
 						Start project?
 					</h2>
 					<h3 className="text-sm text-gray-400 sm:text-xl">
