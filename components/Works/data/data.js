a65ba28afa16a06c04b2d972acd48031ea77aa2d
Muhammad <huzaifakhawar100@gmail.com>
2024-10-31 13:06:28 +0500

    first commit

---
+++ components/Works/data/data.js
@@ -0,0 +1,128 @@
+// const data = [
+//     {
+//         id: 1,
+//         image_url: "https://cdn.dribbble.com/users/2919663/screenshots/18372585/media/22afc4757735732a96d5a9563202f729.png",
+//         work_title: "Blockchain Platform",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//         genre: "Digital Assets"
+//     },
+//     {
+//         id: 2,
+//         image_url: "https://cdn.dribbble.com/users/702789/screenshots/15916650/media/ec203705bbc00f7338ba4e356b2c8af9.png",
+//         work_title: "Web Application",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//         genre: "Design & Dev"
+//     },
+//     {
+//         id: 3,
+//         image_url: "https://cdn.dribbble.com/users/2417352/screenshots/6918204/media/d88f537661e3ea9cc4802e31b06e0ad8.png",
+//         work_title: "Site Optimization",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//         genre: "Interactive guide"
+//     },
+//     {
+//         id: 4,
+//         image_url: "https://cdn.dribbble.com/users/2417352/screenshots/6918204/media/d88f537661e3ea9cc4802e31b06e0ad8.png",
+//         work_title: "Site Optimization",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//         genre: "Interactive guide"
+//     },
+//     {
+//         id: 5,
+//         image_url: "https://cdn.dribbble.com/users/2919663/screenshots/18372585/media/22afc4757735732a96d5a9563202f729.png",
+//         work_title: "Blockchain Platform",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//         genre: "Digital Assets"
+//     },
+//     {
+//         id: 6,
+//         image_url: "https://cdn.dribbble.com/users/702789/screenshots/15916650/media/ec203705bbc00f7338ba4e356b2c8af9.png",
+//         work_title: "Web Application",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//         genre: "Design & Dev"
+//     },
+// ];
+
+const data = [
+	{
+		id: 1,
+		image_url:
+			"https://res.cloudinary.com/dvycqni2r/image/upload/v1730353070/user-auth-app_xajed2.png",
+		work_title: "Full-Stack User Authentication App",
+		description:
+			"A robust user authentication application with full profile management capabilities. Users can securely create accounts, log in, view and edit their profiles, change passwords, and reset forgotten passwords. Additionally, users can view profiles of other logged-in users. The app includes secure, encrypted data handling and responsive design for seamless use across devices.",
+		genre: "Full Stack Development",
+		deploye_url: "https://user-auth-app-five.vercel.app/",
+		is_deployed: true,
+	},
+	{
+		id: 2,
+		image_url:
+			"https://res.cloudinary.com/dvycqni2r/image/upload/v1730353534/e-commerce-app_hdgbnb.png",
+		work_title: "E-commerce App with Zustand and CRUD Functionality",
+		description:
+			"A fully-featured React e-commerce application offering a seamless shopping experience. This app includes product management with CRUD operations, a customizable template for unique branding, and a robust cart system. State management is handled with the Zustand library, ensuring fast and efficient state updates across the application.",
+		genre: "E-commerce Development",
+		deploye_url: "https://e-commerce-react-lilac-five.vercel.app/",
+		is_deployed: true,
+	},
+	{
+		id: 3,
+		image_url:
+			"https://res.cloudinary.com/dvycqni2r/image/upload/v1730354491/master-mind_qptikb.png",
+		work_title: "Mastermind Game | JS",
+		description:
+			"A classic code-breaking game built with JavaScript, HTML, and CSS. Players try to guess a hidden color code within a limited number of attempts, receiving feedback on each guess to help them deduce the correct sequence. The game features dynamic UI elements, responsive design, and interactive feedback, making for an engaging puzzle-solving experience.",
+		genre: "Game Development",
+		deploye_url: "https://master-mind-game.vercel.app/",
+		is_deployed: true,
+	},
+	{
+		id: 4,
+		image_url:
+			"https://res.cloudinary.com/dvycqni2r/image/upload/v1730355239/figma-to-html_vpxvaa.png",
+		work_title: "Figma to HTML | CSS",
+		description:
+			"Convert Figma designs into a responsive HTML and CSS layout with engaging animations and GIFs. This project features a classic code-breaking game built with JavaScript, HTML, and CSS. Players try to guess a hidden color code within a limited number of attempts, receiving feedback on each guess to help them deduce the correct sequence. The game showcases dynamic UI elements, responsive design, and interactive feedback, creating an engaging puzzle-solving experience.",
+		genre: "Web Development",
+		deploye_url: "https://quick-pick-buggcy.vercel.app/",
+		is_deployed: true,
+	},
+	{
+		id: 5,
+		image_url:
+			"https://res.cloudinary.com/dvycqni2r/image/upload/v1730356524/age-calculator_iwtbf7.png",
+		work_title: "Simple Age Calculator",
+		description:
+			"A simple web application built with HTML, CSS, and JavaScript to calculate a user's age based on their date of birth. Users can input their birth date, and the application will display their age in years, months, and days. The project features a clean and responsive design, providing an easy-to-use interface with real-time age calculation as the user inputs their birth date.",
+		genre: "Web Development",
+		deploye_url: "https://age-caclulator-nu.vercel.app/",
+		is_deployed: true,
+	},
+	{
+		id: 6,
+		image_url:
+			"https://res.cloudinary.com/dvycqni2r/image/upload/v1730358716/attendance-system_bswuh0.png",
+		work_title: "Attendance Management System",
+		description:
+			"A Windows Forms application for efficiently managing student attendance. It enables administrators to perform CRUD operations on student records and includes a secure admin login for data protection. With an intuitive interface, it streamlines the attendance management process for educational institutions.",
+		genre: "Desktop Application Development",
+		deploye_url:
+			"https://github.com/Muhammad-Huzaifa24/Employee_Attendance_System_EAD_PreMidProject/",
+		is_deployed: false,
+	},
+	{
+		id: 7,
+		image_url:
+			"https://res.cloudinary.com/dvycqni2r/image/upload/v1730359798/student-interest-system_knhoxe.png",
+		work_title: "Student Interest System",
+		description:
+			"An ASP.NET Core MVC application that allows students to express and manage their interests effectively. The system leverages ADO.NET with MSSQL for data management and Firebase for login / sign Up. It provides a user-friendly interface for both students and administrators, ensuring efficient data handling and seamless user experience.",
+		genre: "Web Application Development",
+		deploye_url:
+			"https://github.com/Muhammad-Huzaifa24/BCSF20M024-EAD-Project-Student-Interest-System",
+		is_deployed: false,
+	},
+];
+
+export default data;
