f1ceb1c4b39a7359977c19aa3afd9fe6d3096642
Muhammad Huzaifa <huzaifakhawar100@gmail.com>
2025-08-03 20:46:01 +0500

    update gitignore

---
+++ constants.ts
@@ -0,0 +1,8 @@
+export const SOCIAL_LINK = {
+    instagram : "https://instagram.com/huzaifyyy__24",
+    facebook : "https://www.facebook.com/huzaifa.khawar.73",
+    twitter : "https://x.com/WorldLenns",
+    github  : "https://github.com/Muhammad-Huzaifa24",
+    linkedIn : "https://www.linkedin.com/in/muhammad-huzaifa-569794290/"
+}
+export const RESUME_LINK = "https://drive.google.com/file/d/19XF2wPV7M2MkUAN52bTQWBMtNygNuYE7/view?usp=sharing";
